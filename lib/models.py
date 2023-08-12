188d552aae487303f3b814efc8254558ad01e16a
Ngoc Nguyen <nnpk0410@gmail.com>
2023-08-12 13:14:18 -0500

    Add Item class to create items table

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -14,4 +14,21 @@ class User(Base):
 
     def __repr__(self):
 
-        return f"<User {self.name} {self.registration_date}>"
+        return f"<User {self.id} {self.name} {self.registration_date}>"
+
+
+class Item(Base):
+
+    __tablename__ = "items"
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    description = Column(String())
+    price = Column(Integer())
+    listing_date = Column(DateTime, default=func.now())
+
+    seller_id = Column(Integer(), ForeignKey("users.id"))
+
+    def __repr__(self):
+
+        return f"<Item {self.id} {self.title} {self.description} {self.price} {self.listing_date} {self.seller_id}>"
