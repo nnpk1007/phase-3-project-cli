bbf65a26491469e198051f5f9ba4ee042fcd046e
Ngoc Nguyen <nnpk0410@gmail.com>
2023-08-18 11:47:38 -0500

    create cli.py

---
+++ lib/cli.py
@@ -0,0 +1,12 @@
+import re
+from prettycli import red
+from simple_term_menu import TerminalMenu
+from models import User, Item, Transaction
+
+
+class Cli():
+
+    def __init__(self):
+        user = None
+
+    
