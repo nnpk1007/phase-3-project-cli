c3f970894f5d3f3dba32390550e796112111d6aa
Ngoc Nguyen <nnpk0410@gmail.com>
2023-08-18 12:03:23 -0500

    add start method in Cli class

--- lib/cli.py
+++ lib/cli.py
@@ -7,6 +7,18 @@ from models import User, Item, Transaction
 class Cli():
 
     def __init__(self):
+        
         user = None
 
     
+    def start(self):
+        
+        print("WELCOME TO FLATIRON MARKET PLACE")
+        options = ["Login", "Sign Up", "Exit"]
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
+
+
+    
+app = Cli()
+app.start()
